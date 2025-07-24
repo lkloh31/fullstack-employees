c09ad9463e36edc908dbd83f3adb65650db869f3
Lokkee Loh <lkloh31@gmail.com>
2025-07-24 10:36:46 -0700

    created employees table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
 DROP TABLE IF EXISTS employees;
 
 
+\echo Creating table.
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name text NOT NULL,
+    birthday date NOT NULL,
+    salary integer NOT NULL
+);
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
+import { faker } from "@faker-js/faker";
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +9,14 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.past({ years: 30 }),
+      salary: faker.number.int({ min: 80000, max: 200000 }),
+    };
+    await createEmployee(employee);
+  }
+
+  console.log("Database seeded successfully");
 }
